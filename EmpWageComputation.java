ba6fab32576b4ef4cc11faa89646054eb26762c6
mukulBillore <mukulbillore88@gmail.com>
2021-12-10 22:12:14 +0530

    modified : adding feature sucessfull

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,6 +1,20 @@
+import java.util.Random;
 public class EmpWageComputation{
 public static void main(String args[]){
 //@start
 System.out.println("Welcome to emp wage computation program on master branch");
+//@uc1
+Random rand= new  Random();
+final int fullDayTime = 8;
+int presentOrAbsent = rand.nextInt(2) ;
+int dailyWorkHours= rand.nextInt(9) ;
+if(presentOrAbsent==1) {
+System.out.println("employ is Present");
+System.out.println("This employ's totel working hour is :-"+dailyWorkHours+" so employs wage is :- "+dailyWorkHours*20);//wage per hour is 20 given 
+}
+else 
+	System.out.println("Employ is absent");
 }
 }
+
+
